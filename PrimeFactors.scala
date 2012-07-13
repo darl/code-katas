da5cae6dbdccf778c3055c6a0a0dfaabb8c362e0
Vladislav Dolbilov <darl@yandex-team.ru>
2012-07-14 01:14:00 +0400

    converted tabs to spaces

--- PrimeFactors.scala
+++ PrimeFactors.scala
@@ -9,37 +9,37 @@ http://amirrajan.net/Blog/code-katas-prime-factors
 import scala.annotation.tailrec
 
 object PrimeFactors extends App {
-	@tailrec private def expand(num: Int, i: Int, result: List[Int]): List[Int] = {
-		if (num > 1)
-			if (num % i == 0) 
-				expand(num / i, i, i :: result)
-			else
-				expand(num, i + 1, result)
-		else 
-			result reverse
-	}
+  @tailrec private def expand(num: Int, i: Int, result: List[Int]): List[Int] = {
+    if (num > 1)
+      if (num % i == 0) 
+        expand(num / i, i, i :: result)
+      else
+        expand(num, i + 1, result)
+    else 
+      result reverse
+  }
 
-	def expand (num: Int): List[Int] = {
-		if (num == Int.MaxValue) 
-			List(Int.MaxValue)
-		else 
-			expand(num, 2, List())
-	}	
+  def expand (num: Int): List[Int] = {
+    if (num == Int.MaxValue) 
+      List(Int.MaxValue)
+    else 
+      expand(num, 2, List())
+  }   
 
-	def myAssert(actual: Any, expected: Any, message: String) {
-		assert(expected == actual, "%s - failed. Expected: %s, actual: %s".format(message, expected, actual))
-		Console.println(message.formatted("%s - ok"))
-	}
+  def myAssert(actual: Any, expected: Any, message: String) {
+    assert(expected == actual, "%s - failed. Expected: %s, actual: %s".format(message, expected, actual))
+    Console.println(message.formatted("%s - ok"))
+  }
 
-	myAssert(expand(0), List(), "0")
-	myAssert(expand(1), List(), "1")
-	myAssert(expand(2), List(2), "2")
-	myAssert(expand(3), List(3), "3")
-	myAssert(expand(4), List(2,2), "4")
-	myAssert(expand(5), List(5), "5")
-	myAssert(expand(6), List(2,3), "6")
-	myAssert(expand(9), List(3,3), "9")
-	myAssert(expand(Int.MaxValue), List(Int.MaxValue), "MaxInt")
+  myAssert(expand(0), List(), "0")
+  myAssert(expand(1), List(), "1")
+  myAssert(expand(2), List(2), "2")
+  myAssert(expand(3), List(3), "3")
+  myAssert(expand(4), List(2,2), "4")
+  myAssert(expand(5), List(5), "5")
+  myAssert(expand(6), List(2,3), "6")
+  myAssert(expand(9), List(3,3), "9")
+  myAssert(expand(Int.MaxValue), List(Int.MaxValue), "MaxInt")
 }
 
 PrimeFactors.main(args)
